de480d0cc47da20c6ad592f05d042605a30c49ec
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-01-03 18:22:08 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var portfolio = document.getElementById("portfolio");
+portfolio.addEventListener('click', aviso);
+
+function aviso(e){
+    alert("Desculpe, Meu Portfolio está em desenvolvimento ainda, caso queira ver meus projetos, estão disponiveis no meu Github!")
+}
