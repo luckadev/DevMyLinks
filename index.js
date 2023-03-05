42ddd5422732560d0ea5fd30285119ebf9cc2c9a
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-03-04 22:28:47 -0300

    version 2.1

--- index.js
+++
@@ -1,6 +0,0 @@
-var portfolio = document.getElementById("portfolio");
-portfolio.addEventListener("click", aviso);
-
-function aviso(e){
-    alert("Desculpe, Meu Portfolio está em desenvolvimento ainda, caso queira ver meus projetos, estão disponiveis no meu Github!");
-}
